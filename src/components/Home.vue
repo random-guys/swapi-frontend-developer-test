c3004ce016129248dac1a3bb2dda0c1cf55d4803
tejiri4 <tejirimatthew4@gmail.com>
2019-09-26 08:13:48 +0100

    create filter component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,58 @@
 <template>
   <div>
     <Search />
+      <div class="title">
+        <div class="title-details">Starwars Characters</div>
+        <div class="line"></div>
+      </div>
+    <div class="select-group">
+      <Select description="FILTER" :options="filterOptions"/>
+      <Select description="VIEW" :options="viewOptions"/>
+    </div>
   </div>
 </template>
 <script>
 import Search from './search/Search';
+import Select from './select/Select';
 
 export default {
   name: 'Home',
   data() {
-    return {};
+    return {
+      filterOptions: [{ detail: 'Male' }],
+      viewOptions: [{ detail: 'Grid' }],
+    };
   },
   components: {
     Search,
+    Select,
   },
 };
 </script>
 <style scoped >
+.title {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  margin-top: 4rem;
+}
+
+.title-details {
+  font-size: 2.0rem;
+  font-weight: bold;
+}
+
+.title .line {
+  height: 0.4rem;
+  background-color: #2c3e50;
+  width: 7rem;
+  margin: 0 auto;
+  margin-top: 1rem;
+}
+
+.select-group {
+  margin: 2rem 7rem;
+  display: flex;
+}
 </style>
---
+++ src/components/select/Select.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="select">
+    <div class="select-description">{{ description }}</div>
+    <div>
+      <select>
+        <option class="selected" v-for="(option, index) in options" v-bind:key="index">
+          {{ option.detail }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Select',
+  props: ['description', 'options'],
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.select {
+  display: flex;
+  align-items: center;
+  margin-right: 2rem;
+}
+
+.select-description {
+  font-weight: 400;
+  font-size: 1.1rem;
+}
+
+.select-categories {
+  margin: 2rem 7rem;
+  display: flex;
+}
+
+select {
+  outline: none;
+  font-size: 1rem;
+  border: 0.1rem solid #2c3e50;
+  border-radius: 0.3rem;
+  padding: 0.6rem 3.2rem;
+  margin-left: 1rem;
+}
+</style>
